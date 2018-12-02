8a0825e7ceecc84e4e6eb46ad189898efab52c75
Abdessamad MOUDARIR <amoudarir@gmail.com>
2018-12-02 00:21:26 +0000

    Adding timezone constant.

--- src/Statics.php
+++ src/Statics.php
@@ -6,5 +6,6 @@ class Statics {
     const API_VERSION   = '1.1';
     const BASE_URI_DEV  = 'http://preprod.binga.ma:8080';
     const BASE_URI_PROD = 'http://api.binga.ma';
+    const TIMEZONE      = 'GMT';
 
 }
