36bdf2c85297b2b078e6dceb880d60d8cb68938e
Abdessamad MOUDARIR <amoudarir@gmail.com>
2018-12-02 00:11:51 +0000

    Adding "Statics" class for constants

---
+++ src/Statics.php
@@ -0,0 +1,10 @@
+<?php
+namespace Moudarir\Binga;
+
+class Statics {
+
+    const API_VERSION   = '1.1';
+    const BASE_URI_DEV  = 'http://preprod.binga.ma:8080';
+    const BASE_URI_PROD = 'http://api.binga.ma';
+
+}
